516985783ccda2c2bebf15efe581d0197ebaa49c
initstring <26131150+initstring@users.noreply.github.com>
2019-07-02 10:38:33 +1000

    added Google App Engine support

--- README.md
+++ README.md
@@ -17,6 +17,7 @@ Currently enumerates the following:
 **Google Cloud Platform**
 - Open GCP Buckets
 - Protected GCP Buckets
+- Google App Engine sites
 
 By "open" buckets/containers, I mean those that allow anonymous users to list contents. if you discover a protected bucket/container, it is still worth trying to brute force the contents with another tool.
 
--- enum_tools/gcp_checks.py
+++ enum_tools/gcp_checks.py
@@ -13,6 +13,7 @@ BANNER = '''
 
 # Known S3 domain names
 GCP_URL = 'storage.googleapis.com'
+APPSPOT_URL = 'appspot.com'
 
 def print_bucket_response(reply):
     """
@@ -57,11 +58,55 @@ def check_gcp_buckets(names, threads):
     # Stop the time
     utils.stop_timer(start_time)
 
+def print_appspot_response(reply):
+    """
+    Parses the HTTP reply of a brute-force attempt
+
+    This function is passed into the class object so we can view results
+    in real-time.
+    """
+    if reply.status_code == 404:
+        pass
+    elif reply.status_code == 200 or repl.status_code == 302:
+        utils.printc("    Google App Engine app: {}\n"
+                     .format(reply.url), 'green')
+    else: print("    Unknown status codes being received:\n"
+                "       {}: {}"
+                .format(reply.status_code, reply.reason))
+
+def check_appspot(names, threads):
+    """
+    Checks for Google App Engine sites running on appspot.com
+    """
+    print("[+] Checking for Google App Engine apps")
+
+    # Start a counter to report on elapsed time
+    start_time = utils.start_timer()
+
+    # Initialize the list of correctly formatted urls
+    candidates = []
+
+    # Take each mutated keyword craft a url with the correct format
+    for name in names:
+        # App Engine project names cannot include a period. We'll exlcude
+        # those from the global candidates list
+        if '.' not in name:
+            candidates.append('{}.{}'.format(name, APPSPOT_URL))
+
+    # Send the valid names to the batch HTTP processor
+    utils.get_url_batch(candidates, use_ssl=False,
+                        callback=print_appspot_response,
+                        threads=threads)
+
+    # Stop the time
+    utils.stop_timer(start_time)
+
 def run_all(names, threads):
     """
     Function is called by main program
     """
     print(BANNER)
 
-    check_gcp_buckets(names, threads)
+    #check_gcp_buckets(names, threads)
+    check_appspot(names, threads)
     return ''
