82c2c977ee4490032267106bf045526a4af9656d
initstring <26131150+initstring@users.noreply.github.com>
2020-08-25 12:06:33 +1000

    moving brute parsing to utils

--- enum_tools/azure_checks.py
+++ enum_tools/azure_checks.py
@@ -151,18 +151,7 @@ def brute_force_containers(storage_accounts, brute_list, threads):
             valid_accounts.append(account)
 
     # Read the brute force file into memory
-    with open(brute_list, encoding="utf8", errors="ignore") as infile:
-        names = infile.read().splitlines()
-
-    # Clean up the names to usable for containers
-    banned_chars = re.compile('[^a-z0-9-]')
-    clean_names = []
-    for name in names:
-        name = name.lower()
-        name = banned_chars.sub('', name)
-        if 63 >= len(name) >= 3:
-            if name not in clean_names:
-                clean_names.append(name)
+    clean_names = utils.get_brute(brute_list, mini=3)
 
     # Start a counter to report on elapsed time
     start_time = utils.start_timer()
