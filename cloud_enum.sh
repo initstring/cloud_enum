cc4f71faae676541f7df59971d4c16fc122dfbc6
initstring <26131150+initstring@users.noreply.github.com>
2022-06-11 15:36:30 +1000

    set PYTHONPATH in shell script

--- cloud_enum.sh
+++ cloud_enum.sh
@@ -3,4 +3,4 @@
 # This is a convenience script for use with a non-installed
 # version of cloud_enum, such as one cloned from a git repo.
 
-python3 cloud_enum/main.py "$@"
+PYTHONPATH=$PYTHONPATH:$(pwd) python3 cloud_enum/main.py "$@"
