c5423738dcb450b9b1282bd004c3170ccd45a856
initstring <26131150+initstring@users.noreply.github.com>
2024-07-09 07:49:19 +0000

    Change default resolver to CF

--- cloud_enum.py
+++ cloud_enum.py
@@ -63,7 +63,7 @@ def parse_arguments():
                         ' Default = 5')
 
     parser.add_argument('-ns', '--nameserver', type=str, action='store',
-                        default='8.8.8.8',
+                        default='1.1.1.1',
                         help='DNS server to use in brute-force.')
     parser.add_argument('-nsf', '--nameserverfile', type=str, 
                         help='Path to the file containing nameserver IPs')
